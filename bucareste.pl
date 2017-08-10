a14bccc9e0194b2fbf6d16a3b464496ee8577d91
MayzaHirose <maayhirose@gmail.com>
2017-08-10 18:21:56 -0300

    N-Rainhas e Bucareste em prolog

---
+++ bucareste.pl
@@ -0,0 +1,104 @@
+﻿% Mayza Yuri Hirose da Costa RA88739
+% Tadao Hara RA89935
+
+:- use_module(library(plunit)).
+
+:- begin_tests(menor_custo).
+test(menor_custo1, V ==	arad) :- menor_custo([d(arad, 75), d(oradea, 71)], V).
+test(menor_custo2, V == urziceni) :- menor_custo([d(iasi, 92), d(urziceni, 142)], V).
+test(menor_custo2, V == rimnicu_vilcea) :- menor_custo([d(arad, 140), d(fagaras, 99), d(oradea, 151), d(rimnicu_vilcea, 80)], V).
+:- end_tests(menor_custo).
+
+vizinhos(arad, [d(sibiu,140), d(timisoara,118), d(zerind,75)]).
+vizinhos(bucharest, [d(fagaras,211), d(giurgiu,90), d(pitesti,101), d(urziceni,85)]).
+vizinhos(craiova, [d(dobreta,120), d(pitesti,138), d(rimnicu_vilcea,146)]).
+vizinhos(dobreta, [d(craiova,120), d(mehadia,75)]).
+vizinhos(eforie, [d(hirsova,86)]).
+vizinhos(fagaras, [d(bucharest,211), d(sibiu,99)]).
+vizinhos(giurgiu, [d(bucharest,90)]).
+vizinhos(hirsova, [d(eforie,86), d(urziceni, 98)]).
+vizinhos(iasi, [d(neamt, 87), d(vaslui, 92)]).
+vizinhos(lugoj, [d(mehadia, 70), d(timisoara, 111)]).
+vizinhos(mehadia, [d(dobreta, 75), d(lugoj, 70)]).
+vizinhos(neamt, [d(iasi, 87)]).
+vizinhos(oradea, [d(sibiu, 151), d(zerind, 71)]).
+vizinhos(pitesti, [d(bucharest, 101), d(craiova, 138), d(rimnicu_vilcea, 97) ]).
+vizinhos(rimnicu_vilcea, [d(craiova, 146), d(pitesti, 97), d(sibiu, 80)]).
+vizinhos(sibiu, [d(arad, 140), d(fagaras, 99), d(oradea, 151), d(rimnicu_vilcea, 80)]).
+vizinhos(timisoara, [d(arad, 118), d(lugoj, 111)]).
+vizinhos(urziceni, [d(bucharest, 85), d(hirsova, 98), d(vaslui, 142)]).
+vizinhos(vaslui, [d(iasi, 92), d(urziceni, 142)]).
+vizinhos(zerind, [d(arad, 75), d(oradea, 71)]).
+
+heuristica(arad, 366).
+heuristica(bucharest, 0).
+heuristica(craiova, 160).
+heuristica(dobreta, 238).
+heuristica(eforie, 161).
+heuristica(fagaras, 178).
+heuristica(giurgiu, 77).
+heuristica(hirsova, 151).
+heuristica(iasi, 226).
+heuristica(lugoj, 244).
+heuristica(mehadia, 241).
+heuristica(neamt, 234).
+heuristica(oradea, 380).
+heuristica(pitesti, 98).
+heuristica(rimnicu_vilcea, 193).
+heuristica(sibiu, 253).
+heuristica(timisoara, 329).
+heuristica(urziceni, 80).
+heuristica(vaslui, 199).
+heuristica(zerind, 374).
+
+%% melhor_caminho(?O, ?C) is nondet
+%  Verdadeiro se C é o melhor caminho entre O e bucharest
+%  Exemplo:
+%    ?- melhor_caminho(sibiu, C).
+%    C = [sibiu, rimnicu_vilcea, pitesti, bucharest].
+melhor_caminho(bucharest, [bucharest]) :- !.
+melhor_caminho(O, [O|Cs]) :-
+	melhor_vizinho(O, V), !,
+	melhor_caminho(V, Cs).
+
+
+%% melhor_vizinho(?O, ?V) is nondet
+%  Verdadeiro se V é o melhor vizinho de O
+%  Exemplo:
+%    ?- melhor_vizinho(sibiu, V).
+%    V = rimnicu_vilcea.
+melhor_vizinho(O, V) :-
+	vizinhos(O,OS),
+	menor_custo(OS, V).
+
+%% menor_custo(?Vizinhos, ?Menor_cidade) is det
+%  Verdadeiro se Menor_cidade é a cidade com menor custo dentre os
+%  Vizinhos
+%  Exemplo: ?- menor_custo([d(arad, 75), d(oradea, 71)], V).
+%	    V = arad.
+%	    ?- menor_custo([d(iasi, 92), d(urziceni, 142)], V).
+%	    V = urziceni.
+menor_custo([O|OS], Menor_cidade) :-
+	O = d(Cidade,Dist),
+	heuristica(Cidade,H),
+	Custo is Dist+H,
+	Menor is Custo,
+	menor_custo(OS, Menor, Cidade, Cidade0),
+	Menor_cidade = Cidade0, !.
+
+menor_custo([O|OS], Menor, _, Menor_cidade2) :-
+	O = d(Cidade,Dist),
+	heuristica(Cidade,H),
+	Custo is Dist+H,
+	Menor > Custo,
+	Menor0 is Custo,
+	menor_custo(OS, Menor0, Cidade, Menor_cidade0),
+	Menor_cidade2 = Menor_cidade0.
+
+menor_custo([_|OS], Menor, Menor_cidade, Menor_cidade0) :-
+	menor_custo(OS, Menor, Menor_cidade, Menor_cidade0).
+
+menor_custo([], _, Cidade, Cidade0):-
+	Cidade0 = Cidade.
+
+
---
+++ rainhas.pl
@@ -0,0 +1,204 @@
+﻿% Mayza Yuri Hirose da Costa RA88739
+% Tadao Hara RA89935
+
+:- use_module(library(plunit)).
+
+:- begin_tests(retira_membro).
+test(retira_membro1) :- retira_membro(1,[1,2,3],[2,3]).
+test(retira_membro2, YS == [2,3]) :- retira_membro(1,[1,2,3], YS).
+:- end_tests(retira_membro).
+
+:- begin_tests(nao_membro).
+test(nao_membro1) :- nao_membro(1,[2,3]).
+test(nao_membro2, YS == []) :- nao_membro(1,YS).
+:- end_tests(nao_membro).
+
+:- begin_tests(elementos_distintos).
+test(elementos_distintos1) :- elementos_distintos([1,2,3]).
+test(elementos_distintos2, [fail]) :- elementos_distintos([1,1,2,3]).
+:- end_tests(elementos_distintos).
+
+:- begin_tests(valido_diagonal_cima).
+test(valido_diagonal_cima1) :- valido_diagonal_cima(1,[2,3]).
+test(valido_diagonal_cima2, [fail]) :- valido_diagonal_cima(1,[1,2,3]).
+:- end_tests(valido_diagonal_cima).
+
+:- begin_tests(valido_diagonal_baixo).
+test(valido_diagonal_baixo1) :- valido_diagonal_baixo(1,[2,3]).
+test(valido_diagonal_baixo2, [fail]) :- valido_diagonal_baixo(1,[1,2,3]).
+:- end_tests(valido_diagonal_baixo).
+
+
+% ------------------- VERSÃO GERAR E TESTAR COM PERMUTAÇÕES -------------------
+
+%% rainhas_p(?Q, +N) is nondet.
+%  Verdadeiro se Q é uma solução de tamanho N com N rainhas.
+%  Este predicado constrói as possíves soluções do N-rainhas.
+rainhas_p(Q, N) :-
+	sequencia(1, N, R),
+	permutacao(R, Q),
+	solucao(Q).
+
+
+%% sequencia(+I, +F, ?S) is semidet.
+%  Verdadeiro se S é uma lista com os números inteiros entre I % e
+%  F(inclusive).
+sequencia(F, F, S) :-
+	S = [F],
+	!.
+
+sequencia(I, F, S) :-
+	I0 is I+1,
+	sequencia(I0, F, S0),
+	S = [I|S0].
+
+
+%% permutacao(?L, ?P) is nondet.
+%  Verdadeiro se P é uma permutação da lista L
+permutacao([],[]).
+
+permutacao(L, [P|PS]) :-
+	retira_membro(P,L,L1),
+	permutacao(L1,PS).
+
+%% retira_membro(+X, +XS, ?YS) is nondet.
+%  Verdadeiro se YS é a lista XS mas sem a ocorrência de X.
+%  Exemplo:
+%    ?- retira_membro(1, [1,2,3], YS).
+%    YS = [2,3].
+retira_membro(X, [X | XS], L1) :-
+	L1 = XS.
+
+retira_membro(X, [Y | YS], L1) :-
+	retira_membro(X, YS, L2),
+	L1 = [Y|L2].
+
+
+
+%% -------------------------- VERSÃO COM BACKTRACKING --------------------------
+
+% rainhas_n(?Q, +N) is nondet.
+% predicado "interface" para o rainhas/3
+rainhas_n(Q, N) :-
+	rainhas_n(Q, N, N).
+
+
+%% rainhas_n(?Q, +N, +T) is nondet.
+%  Verdadeiro se Q é uma solução de tamanho T com N rainhas.
+%  Este é um predicado auxiliar e não deve ser chamado diretamente.
+%  Sua prova deve ser realizada com N = T.
+%  Exemplo:
+%    ?- rainhas_n(Q, 5, 5).
+%    Q = [4, 2, 5, 3, 1] ;
+%    Q = [3, 5, 2, 4, 1] ;
+%    Q = [5, 3, 1, 4, 2] ;
+%    ...
+rainhas_n([R|Rs], N, T) :-
+	T > 0, !,
+	T0 is T-1,
+	rainhas_n(Rs, N, T0),
+	entre(1, N, R),
+	solucao([R|Rs]).
+
+rainhas_n([], _, 0).
+
+
+
+%% entre(+I, +F, ?V) is nondet.
+%  Verdadeiro se V é um número natural entre I e F (inclusive).
+%  Exemplo:
+%    ?- entre(1, 3, V).
+%    V = 1;
+%    V = 2;
+%    V = 3;
+%    false.
+entre(I,F,V):-
+	I < F,
+	V = I.
+
+entre(F,F,V):-
+	!,
+	V = F.
+
+entre(I,F,V):-
+	I < F,
+	I0 is I+1,
+	entre(I0,F,V).
+
+%% solucao(+Q) is semidet.
+%  Verdadeiro se Q é uma solução N-rainhas
+%  Este predicado apenas verifica se Q é uma solução, e não a constrói.
+%  Exemplo:
+%    ?- solucao([4, 5, 3, 2, 1]).
+%    true.
+solucao([Q1|QS]):-
+	elementos_distintos([Q1|QS]),
+	Q0 is Q1-1,
+	valido_diagonal_cima(Q0, QS),
+	Q2 is Q1+1,
+	valido_diagonal_baixo(Q2, QS),
+	solucao(QS).
+
+solucao([_]):-!.
+
+%% nao_membro(+X, +XS) is det.
+%  Verdadeiro se XS é uma lista sem a ocorrência de X.
+%  Exemplo:
+%    ?- nao_membro(1, [1,2,3]).
+%    false.
+%    ?- nao_membro(1, [2,3]).
+%    true.
+nao_membro(_, []):-
+	!.
+
+nao_membro(X, [Y | XS]) :-
+	X =\= Y,
+	nao_membro(X, XS).
+
+%% elementos_distintos(+XS) is det.
+%  Verdadeiro se XS é uma lista sem repeticoes
+%  Exemplo:
+%    ?- elementos_distintos([1,2,3]).
+%    true.
+%    ?- elementos_distintos([1,1,2,3]).
+%    false.
+elementos_distintos([]).
+
+elementos_distintos([A|AS]):-
+	nao_membro(A, AS),
+	elementos_distintos(AS).
+
+%% valido_diagonal_cima(+X, +XS) is det.
+%  Verdadeiro se XS possui a diagonal de cima livre em relacao a X.
+%  Verifica recursivamente se, em relacao à rainha atual, haverá uma
+%  rainha na sua diagonal mais para frente.
+%
+%  Exemplo:
+%    ?- valido_diagonal_cima(1,[2,3]).
+%    true.
+%    ?- valido_diagonal_cima(1,[1,2,3]).
+%    false.
+valido_diagonal_cima(Q0,[Q|QS]):-
+	Q =\= Q0,
+	Q1 is Q0-1,
+	valido_diagonal_cima(Q1,QS), !.
+
+valido_diagonal_cima(_,[]).
+
+%% valido_diagonal_baixo(+X, +XS) is det.
+%  Verdadeiro se XS possui a diagonal de baixo livre em relacao a X.
+%  Verifica recursivamente se, em relacao à rainha atual, haverá uma
+%  rainha na sua diagonal mais para frente.
+%
+%  Exemplo:
+%    ?- valido_diagonal_baixo(1,[2,3]).
+%    true.
+%    ?- valido_diagonal_baixo(1,[1,2,3]).
+%    false.
+valido_diagonal_baixo(Q0,[Q|QS]):-
+	Q =\= Q0,
+	Q1 is Q0+1,
+	valido_diagonal_baixo(Q1,QS), !.
+
+valido_diagonal_baixo(_,[]).
+
